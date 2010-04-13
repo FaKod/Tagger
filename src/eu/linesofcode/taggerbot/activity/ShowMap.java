aa8b4596daa15548eaad9f84df09efd1d5d9dc80
Robert <xperimental@solidproject.de>
2010-04-13 18:26:21 +0000

    Added new activity for creating location tags.

--- src/eu/linesofcode/taggerbot/activity/ShowMap.java
+++ src/eu/linesofcode/taggerbot/activity/ShowMap.java
@@ -34,11 +34,7 @@ import eu.linesofcode.taggerbot.NetworkState;
 import eu.linesofcode.taggerbot.Prefs;
 import eu.linesofcode.taggerbot.R;
 import eu.linesofcode.taggerbot.client.TaggerClient;
-import eu.linesofcode.taggerbot.client.data.ELocationTag;
-import eu.linesofcode.taggerbot.client.data.LocationType;
-import eu.linesofcode.taggerbot.client.data.Tlocation;
 import eu.linesofcode.taggerbot.client.data.Tlocationtag;
-import eu.linesofcode.taggerbot.client.data.Tuser;
 import eu.linesofcode.taggerbot.map.LocationTagOverlay;
 import eu.linesofcode.taggerbot.map.TagOverlayListener;
 import eu.linesofcode.taggerbot.service.LocationUpdateService;
@@ -226,46 +222,9 @@ public class ShowMap extends MapActivity {
     private void createTagCurrentLocation() {
         Location currentLocation = ClientState.getState().getCurrentLocation();
         if (currentLocation != null) {
-            Tlocation tagLocation = new Tlocation();
-            tagLocation.setLocationType(LocationType.LocationTag.toString());
-            tagLocation.setLatitude(currentLocation.getLatitude());
-            tagLocation.setLongitude(currentLocation.getLongitude());
-
-            // TODO Ask user for name and text
-            Tlocationtag tag = new Tlocationtag();
-            tag.setName("Name");
-            tag.setInfotext("Infotext");
-            tag.setPoint(tagLocation);
-
-            final ELocationTag data = new ELocationTag();
-            data.setLocationTag(tag);
-
-            if (ClientState.getState().isConnected()) {
-                worker.submit(new Runnable() {
-
-                    @Override
-                    public void run() {
-                        ClientTask task = new ClientTask() {
-
-                            @Override
-                            public boolean run(TaggerClient client) {
-                                Tuser me = client.user().get();
-                                Tlocationtag tag = client.tags().create(data,
-                                        Integer.parseInt(me.getId()));
-                                List<Tlocationtag> added = new ArrayList<Tlocationtag>();
-                                added.add(tag);
-                                ClientState.getState().modifyOwnTags(added,
-                                        new ArrayList<Tlocationtag>());
-                                return true;
-                            }
-                        };
-                        ClientState.getState().doTask(task);
-                    }
-                });
-            } else {
-                Toast.makeText(this, R.string.showmap_toast_nonetwork,
-                        Toast.LENGTH_LONG).show();
-            }
+            Intent i = new Intent(this, CreateTag.class);
+            i.putExtra(CreateTag.EXTRA_LOCATION, currentLocation);
+            startActivity(i);
         } else {
             Toast.makeText(this, R.string.showmap_toast_nolocation,
                     Toast.LENGTH_LONG).show();
